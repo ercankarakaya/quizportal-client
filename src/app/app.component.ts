abf5fae6a43909cce270aceda4f9e1185ced7c68
Ercan Karakaya <karakayaercan71@gmail.com>
2022-04-16 16:46:08 +0300

    Page title operations have been done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'quizportal-client';
+  //title:'QuizPortal';
+
+  constructor(private router: Router, private titleService: Title) {
+    //this.titleService.setTitle(title);
+  }
+
+  ngOnInit() {
+    this.getPageTitle();
+  }
+
+  getPageTitle() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let route: ActivatedRoute = this.router.routerState.root;
+          let routTitle = '';
+          while (route!.firstChild) {
+            route = route.firstChild;
+          }
+          if (route.snapshot.data['title']) {
+            routTitle = route!.snapshot.data['title'];
+          }
+          return routTitle;
+        })
+      )
+      .subscribe((title: string) => {
+        if (title) {
+          this.titleService.setTitle(`${title}`);
+        }
+      });
+  }
 }
